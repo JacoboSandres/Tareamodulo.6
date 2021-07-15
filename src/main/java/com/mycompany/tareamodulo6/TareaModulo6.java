53d8fb05c3c59e3630d4c604960f84b4a56d63d1
SALINAS <SALINAS@DESKTOP-25SP577>
2021-07-15 07:25:05 +0200

    Primer commit para repositorio remoto -

---
+++ src/main/java/com/mycompany/tareamodulo6/TareaModulo6.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.tareamodulo6;
+
+
+public class TareaModulo6 {
+
+    public static void main(String[] args){
+    System.out.println("Mi primera vez utilizando Github");  
+}
+
+}
